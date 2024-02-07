d98ff40135c5e1a6444527440167fa2eb3d0cfb0
Albertus van der Walt <albertus@psybergate.co.za>
2024-02-07 14:13:08 +0200

    yeet

--- src/searches.py
+++ src/searches.py
@@ -89,7 +89,7 @@ class Searches:
                 searchbar = self.webdriver.find_element(By.ID, "sb_form_q")
                 searchbar.send_keys(word)
                 searchbar.submit()
-                time.sleep(random.randint(20, 40))
+                time.sleep(random.randint(40, 60))
                 return self.browser.utils.getBingAccountPoints()
             except TimeoutException:
                 logging.error("[BING] " + "Timeout, retrying in 5 seconds...")
